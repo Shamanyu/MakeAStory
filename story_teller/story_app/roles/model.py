72503a7ee00043c1d1616df4e7a09a6d6a220912
shamanyu <shubham.s@greyorange.sg>
2017-05-29 19:26:15 +0530

    Add create_time and update_time fields in roles table

--- story_teller/story_app/roles/model.py
+++ story_teller/story_app/roles/model.py
@@ -7,3 +7,6 @@ class Role(db.Model):
 
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     role_name = db.Column(db.String(256), unique=True)
+    update_time = db.Column(db.DateTime, default=datetime.datetime.utcnow,                                              
+        onupdate=datetime.datetime.utcnow)
+    create_time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
