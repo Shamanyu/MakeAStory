3cc02737f22dd1e6edb7fdae2f0960f96c820779
shamanyu <shubham.s@greyorange.sg>
2017-05-29 19:33:28 +0530

    Add placeholder for forms of all models

---
+++ story_teller/story_app/roles/forms.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+    story_teller.roles.forms
+    ~~~~~~~~~~~~~~~~~~~~
+
+    Role forms
+"""
---
+++ story_teller/story_app/users/forms.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+    story_teller.users.forms
+    ~~~~~~~~~~~~~~~~~~~~
+
+    User forms
+"""
