e8460661edd782202bb5f4fa0801c532e0b0d2e6
shamanyu <shubham.s@greyorange.sg>
2017-05-29 19:42:04 +0530

    Add central models and forms file

---
+++ story_teller/story_app/forms.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    story_teller.forms
+    ~~~~~~~~~~~~~~
+
+    consolidated forms module
+"""
+
+from .users.forms import *
+from .roles.forms import *
+from .stories.forms import *
+from .sentences.forms import *
---
+++ story_teller/story_app/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    overholt.models
+    ~~~~~~~~~~~~~~~
+
+    consolidated models module
+"""
+
+from .users.models import *
+from .roles.models import *
+from .stories.models import *
+from .sentences.models import *
